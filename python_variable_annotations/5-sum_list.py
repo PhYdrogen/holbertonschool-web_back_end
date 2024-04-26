1e569f340481fa1aec46a30228b24a53c9ddc2ab
Gabriel <fossecruor@gmail.com>
2024-04-26 11:45:27 +0200

    added typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+from typing import List
 """
-Complex types - list of floats
+list of floats
 """
 
 
-def sum_list(input_list: list[float]) -> float:
-    """ Complex types - list of floats """
-
+def sum_list(input_list: List[float]) -> float:
+    """ list of floats """
     return sum(input_list)
