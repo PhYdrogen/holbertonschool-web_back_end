9eb0e2bda3771f4de42e254c3743f71c5bd9399c
Gabriel <fossecruor@gmail.com>
2024-04-26 12:02:44 +0200

    trying 2

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from typing import List
 """
-ok
+calculating the sum and returning a float
 """
 
 
-def sum_list(input_list: List[float]) -> float:
-    """ ok """
+def sum_list(input_list: list[float]) -> float:
+    """ calculating the sum and returning a float """
     return sum(input_list)
