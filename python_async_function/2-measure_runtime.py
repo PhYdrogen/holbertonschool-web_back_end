85f5f00603cc2d7e3fa0b3efa3d756e1c0846808
Gabriel <fossecruor@gmail.com>
2024-04-28 16:28:44 +0200

    not a async function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,14 +4,14 @@ Using back the fn and mesuring
 """
 
 import time
+import asyncio
 
-
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ takes 2 int, n, max_delay and output a list of waited time """
 
     wait_n = __import__('1-concurrent_coroutines').wait_n
 
     start = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
 
-    return start - time.time() / n
+    return (time.time() - start) / n
