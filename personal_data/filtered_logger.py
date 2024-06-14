ed0350a1dbc75e22d321b43ee83d2e0ec5250efd
Gabriel <fossecruor@gmail.com>
2024-06-14 13:20:28 +0200

    Added pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """" filtered logger """
+
 from typing import List
 import re
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str):
+    """ this function hide the data from the main """
     patern = "|".join(fields)
     return re.sub(f'({patern})=.*?;', r'\g<1>=' + redaction + ";", message)
