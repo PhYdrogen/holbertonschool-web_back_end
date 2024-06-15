203bc758522b0377dec9a4e13bc7e50ae5321980
Gabriel <fossecruor@gmail.com>
2024-06-15 14:33:16 +0200

    added separator to the fn

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,4 +9,5 @@ def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """ this function hide the data from the main """
     patern = "|".join(fields)
-    return re.sub(f'({patern})=.*?;', r'\g<1>=' + redaction + ";", message)
+    return re.sub(f'({patern})=.*?{separator}', r'\g<1>' + "="
+                  + redaction + separator, message)
