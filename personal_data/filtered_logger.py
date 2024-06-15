446ebd79d784b1c4bbcb249a81a0e72bc0f7f126
Gabriel <fossecruor@gmail.com>
2024-06-15 15:22:23 +0200

    added class and updated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-"""" filtered logger """
+"""" filtered logger with class """
 
-from typing import List
+from typing import List, Tuple
 import re
+import logging
 
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
-    """ this function hide the data from the main """
-    patern = "|".join(fields)
-    return re.sub(f'({patern})=.*?{separator}', r'\g<1>' + "="
-                  + redaction + separator, message)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    FIELDS: List[str] = []
+
+    def __init__(self, fields: Tuple[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        for f in fields:
+            self.FIELDS.append(f)
+
+    def format(self, record: logging.LogRecord) -> str:
+        return self.filter_datum(record.getMessage())
+
+    def filter_datum(self, message: str) -> str:
+        """ this function hide the data from the main """
+        patern = "|".join(self.FIELDS)
+        return re.sub(f'({patern})=.*?{self.SEPARATOR}', r'\g<1>' + "="
+                    + self.REDACTION + self.SEPARATOR, message)
