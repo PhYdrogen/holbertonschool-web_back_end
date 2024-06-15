1424900117b7539eac0ec434271c4bf0acb0e2cf
Gabriel <fossecruor@gmail.com>
2024-06-15 17:55:36 +0200

    added fn

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(pwd: str) -> bytes:
     """ hashing password base on pwd """
     return bcrypt.hashpw(str.encode(pwd), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, pwd: str) -> bool:
+    """ check if password is valid """
+    return bcrypt.checkpw(str.encode(pwd), hashed_password)
