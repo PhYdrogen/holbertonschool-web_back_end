6c2cc86a801447d88857448ab1c677339847e070
Gabriel <fossecruor@gmail.com>
2024-06-15 17:53:01 +0200

    changed type

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,9 +2,8 @@
 """" encrypt password file """
 
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(pwd: str) -> ByteString:
+def hash_password(pwd: str) -> bytes:
     """ hashing password base on pwd """
     return bcrypt.hashpw(str.encode(pwd), bcrypt.gensalt())
