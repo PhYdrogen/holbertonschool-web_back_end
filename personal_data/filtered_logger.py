b90fe8d488f12dc663c1e20c36db6f76d701a99d
Gabriel <fossecruor@gmail.com>
2024-06-15 15:24:03 +0200

    fix pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -16,15 +16,17 @@ class RedactingFormatter(logging.Formatter):
     FIELDS: List[str] = []
 
     def __init__(self, fields: Tuple[str]):
+        """ The init method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         for f in fields:
             self.FIELDS.append(f)
 
     def format(self, record: logging.LogRecord) -> str:
+        """ The format method """
         return self.filter_datum(record.getMessage())
 
     def filter_datum(self, message: str) -> str:
         """ this function hide the data from the main """
         patern = "|".join(self.FIELDS)
         return re.sub(f'({patern})=.*?{self.SEPARATOR}', r'\g<1>' + "="
-                    + self.REDACTION + self.SEPARATOR, message)
+                      + self.REDACTION + self.SEPARATOR, message)
