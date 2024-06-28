f4079e4ce817b1e36c92cda9df59413878acc43c
Gabriel <fossecruor@gmail.com>
2024-06-28 22:39:48 +0200

    ye4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,11 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Check if the request is authenticated. """
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return True
+        if path[-1] == '/':
+            path = path[0: -1]
+        excluded_paths = map(lambda x: x[0:-1] if x[-1] == '/' else x, excluded_paths)
         if path in excluded_paths:
             return False
         return True
