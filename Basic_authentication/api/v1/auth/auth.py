7903527778b42440fd22e169db484afbbdd2c69a
Gabriel <fossecruor@gmail.com>
2024-06-28 18:53:38 +0200

    pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,11 @@ from typing import TypeVar
 class Auth:
     """ A class to manage the API authentication.
     """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool: 
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         return None
