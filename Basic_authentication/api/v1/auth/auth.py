b23a1f556b608d286a39e7a58d4c6bd19d75ce4a
Gabriel <fossecruor@gmail.com>
2024-06-28 19:09:40 +0200

    forgot to add comment......

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,13 @@ class Auth:
     """ A class to manage the API authentication.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Check if the request is authenticated. """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ Get the authorization header. """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ Get the current user. """
         return None
