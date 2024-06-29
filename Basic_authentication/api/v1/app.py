58b5e37a731ed4beb41f962bf54039f681814c1b
Gabriel <fossecruor@gmail.com>
2024-06-29 11:42:29 +0200

    idk bro

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -54,10 +54,10 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     auth = getenv("AUTH_TYPE")
-    if auth == "auth":
-        from api.v1.auth.auth import Auth
-        auth = Auth()
     if auth == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
     app.run(host=host, port=port)
