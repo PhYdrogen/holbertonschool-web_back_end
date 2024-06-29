7f130529bb1e99d9cd08c7f538a7487cc212504b
Gabriel <fossecruor@gmail.com>
2024-06-29 11:50:57 +0200

    big

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Auth file for basic auth class
 """
-from auth import Auth
-
+from api.v1.auth.auth import Auth
+ 
 
 class BasicAuth(Auth):
     """ Basic authentication class
