940d2e0af506fadac0fc909482aa8d9c173c893b
Gabriel <fossecruor@gmail.com>
2024-07-20 09:18:12 +0100

    t2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,9 +8,21 @@ from flask import request
 from typing import List, TypeVar
 import base64
 import binascii
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session authentication class
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
