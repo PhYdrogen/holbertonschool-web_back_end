e002dd06517633f27c8558ebd250f86179347464
Gabriel <fossecruor@gmail.com>
2024-07-20 09:32:30 +0100

    both.

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,7 +59,7 @@ def before_request():
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-            if auth.authorization_header(request) and \
+            if auth.authorization_header(request) is None and \
                auth.session_cookie(request) is None:
                 abort(401)
             request.current_user = auth.current_user(request)
