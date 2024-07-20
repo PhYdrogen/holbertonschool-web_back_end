2f712e1c931e38d98e9bbf06ac171c3ae370fdbf
Gabriel <fossecruor@gmail.com>
2024-07-20 09:21:53 +0100

    t3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -23,6 +23,16 @@ class SessionAuth(Auth):
             return None
         if type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ that returns a User ID based on a Session ID """
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
