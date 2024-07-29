649523a3fee3d02591f8b68609d656b628500afb
Gabriel <fossecruor@gmail.com>
2024-07-29 13:10:11 +0100

    register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,8 @@ AUTH file
 """
 import bcrypt
 from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import uuid
 
 
@@ -23,3 +25,13 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register user """
+        try:
+            self._db.find_user_by(email=email)
+
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hash_pwd)
