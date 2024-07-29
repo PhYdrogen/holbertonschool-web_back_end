3202b0394de7e571367decbcd6e5fe4c0b518011
Gabriel <fossecruor@gmail.com>
2024-07-29 13:11:32 +0100

    destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -69,3 +69,10 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy session id """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except InvalidRequestError:
+            return None
