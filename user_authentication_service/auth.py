6e5365357e8c4969a21ef3d1f5a7b931274a6472
Gabriel <fossecruor@gmail.com>
2024-07-29 13:08:20 +0100

    add generate

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,14 @@
 AUTH file
 """
 import bcrypt
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """ hash password for security """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ generate uuid """
+    return str(uuid.uuid4())
