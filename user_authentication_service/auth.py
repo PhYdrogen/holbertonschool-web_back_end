26e3cf2674aefe9942955e802f9b921f5c1e61a9
Gabriel <fossecruor@gmail.com>
2024-07-30 10:19:09 +0100

    except everything

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -47,7 +47,7 @@ class Auth:
                 return True
             else:
                 return False
-        except NoResultFound:
+        except:
             return False
 
     def create_session(self, email) -> str:
