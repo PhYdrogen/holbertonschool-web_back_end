386cdd85bfc3319908c4c58d269b410d2c1998ee
Gabriel <fossecruor@gmail.com>
2024-08-09 15:51:26 +0100

    ADD VERY USEFULL COMMENT

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,13 +8,14 @@ from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-
+    """Test access_nested_map function"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the expected value"""
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -22,18 +23,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """Test that access_nested_map raises a KeyError when the path is invalid"""
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-
+    """Test get_json function"""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     @patch("requests.get")
     def test_get_json(self, url: str, payload: dict, mock_get: Mock):
+        """Test that get_json returns the expected value"""
         mock_resp = Mock()
         mock_resp.json.return_value = payload
         mock_get.return_value = mock_resp
@@ -41,8 +44,10 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """Test memoization decorator"""
 
     def test_memoize(self):
+        """Test that the memoize decorator works"""
         class TestClass:
             def a_method(self):
                 return 42
