b0d2bf0fcd234be9bf990ce655e4a6289c740bd5
Gabriel <fossecruor@gmail.com>
2024-09-04 19:43:33 +0200

    add local

---
+++ i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Basic word for doc
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """ class Config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ index """
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get local languages """
+    return request.accept_languages.best_match(Config.LANGUAGES)
