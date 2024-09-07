7c833ce3ad83fc49dd907b6589a583060f3f634d
Gabriel <fossecruor@gmail.com>
2024-09-07 13:16:12 +0200

    done

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,49 +1,88 @@
 #!/usr/bin/env python3
-""" Exercice file for redis """
+""" Writing strings to Redis """
 import redis
 import uuid
-from typing import Callable, Union, Optional
 import functools
+from typing import Union, Callable, Optional
 
 
 def count_calls(method: Callable) -> Callable:
-    """Count the number of calls to a function"""
+    """ Count calls decorator """
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrap the function"""
+        """ Wrapper that increments key every time the method is called """
         key = method.__qualname__
         self._redis.incr(key)
+
         return method(self, *args, **kwargs)
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Call history decorator """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper that stores the history of inputs and outputs """
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+
+        return result
+    return wrapper
+
+
+def replay(method: Callable):
+    """ Display the history of calls for a particular function. """
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+    cache = method.__self__._redis
+
+    inputs = cache.lrange(input_key, 0, -1)
+    outputs = cache.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input_data, output_data in zip(inputs, outputs):
+        input_str = input_data.decode('utf-8')
+        output_str = output_data.decode('utf-8')
+        print(f"{method.__qualname__}(*{input_str}) -> {output_str}")
+
+
 class Cache:
-    """A simple cache"""
+    """ Cache class """
 
     def __init__(self) -> None:
-        """Init the cache"""
+        """ Constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store data in the cache"""
+        """ Store data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
+
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None
             ) -> Optional[Union[str, bytes, int, float]]:
-        """Get data from the cache"""
+        """ Get data from Redis """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
         if fn is None:
-            return self._redis.get(key)
-        else:
-            return fn(self._redis.get(key))
+            return data
 
-    def get_str(self, key: str) -> Union[str, None]:
-        """ method to get string """
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
+        return fn(data)
 
-    def get_int(self, key: str) -> Union[int, None]:
-        """ method to get int """
-        return self.get(key, fn=int)
+    def get_str(self, key: str) -> Optional[str]:
+        """ Get a string """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: int) -> Optional[int]:
+        """ Get an int """
+        return self.get(key, int)
