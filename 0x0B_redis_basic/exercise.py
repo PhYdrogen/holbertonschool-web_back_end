778f3c53307b9905fab210b53b9a3ff72b39131c
Gabriel <fossecruor@gmail.com>
2024-09-07 12:25:41 +0200

    more type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,34 +1,33 @@
 """ Exercice file for redis """
-import redis
+from redis import Redis
 from uuid import uuid4
-import typing
+from typing import Callable, Union
 
 class Cache:
     """A simple cache"""
 
     def __init__(self):
-        self._redis = redis.Redis()
+        """Init the cache"""
+        self._redis = Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in the cache"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
     
-    def get(self, key: str, fn: typing.Callable | None) :
+    def get(self, key: str, fn: Callable | None) -> Union[str, bytes, int, float]:
         """Get data from the cache"""
-        if key is None or key == "":
-            return None
         if fn is None:
             return self._redis.get(key)
         else:
             return fn(self._redis.get(key))
 
-    def get_str(self):
+    def get_str(self, key: str) -> Union[str, None]:
         """ method to get string """
-        
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
 
-    def get_int(self):
+    def get_int(self, key: str) -> Union[int, None]:
         """ method to get int """
-
+        return self.get(key, fn=int)
