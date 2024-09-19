92527d4c3bda3585c67f2f748d50fdb1839166bb
Gabriel <fossecruor@gmail.com>
2024-09-19 20:12:38 +0200

    this year?

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- script that lists all bands with Glam rock as their main style, ranked by their longevity
--- if split is NULL split equal to 2020 with INFULL
-SELECT DISTINCT band_name, IFNULL(split, 2020) - formed AS lifespan FROM metal_bands WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
+-- if split is NULL split equal to 2024 with INFULL
+SELECT DISTINCT band_name, IFNULL(split, 2024) - formed AS lifespan FROM metal_bands WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
