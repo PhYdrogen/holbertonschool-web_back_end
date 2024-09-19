06626037ecbd4046b8879ef5c397c57b99bee078
Gabriel <fossecruor@gmail.com>
2024-09-19 20:17:49 +0200

    mysql> delimiter //

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,5 +1,5 @@
 -- script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
-DELIMITER $
+DELIMITER //
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
@@ -9,6 +9,6 @@ BEGIN
     ELSE
         RETURN (a / b);
     END IF;
-END $
+END //
 
 DELIMITER ;
