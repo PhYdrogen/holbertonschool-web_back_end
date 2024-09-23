c48ce8d65b64f5ff72e3f2b71823a2ad2ba68ef6
Gabriel <fossecruor@gmail.com>
2024-09-23 12:41:44 +0200

    idk anymore

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""" this file is printing the output of a dump """
+"""
+a script that provides some stats about a nginx dump logs stored in mongo
+"""
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-c = client.logs.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print(f"{c.estimated_document_count()} logs\nMethods: ")
-for m in method:
-    print(f"\tmethod {m}: {c.count_documents({"method": m})}")
-print(f"{c.count_documents({"path": "/status"})} status check")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    c = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{c.estimated_document_count()} logs\nMethods:")
+    for m in method:
+        print(f"\tmethod {m}: {c.count_documents({"method": m})}")
+    print(f"{c.count_documents({"path": "/status"})} status check")
