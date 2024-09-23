31852e2b43599d84f756757e37945f41502990e3
Gabriel <fossecruor@gmail.com>
2024-09-23 14:33:09 +0200

    space after

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     c = client.logs.nginx
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(f"{c.estimated_document_count()} logs\nMethods:")
+    print(f"{c.estimated_document_count()} logs\nMethods: ")
     for m in method:
         print(f"\tmethod {m}: {c.count_documents({"method": m})}")
     print(f"{c.count_documents({"path": "/status"})} status check")
