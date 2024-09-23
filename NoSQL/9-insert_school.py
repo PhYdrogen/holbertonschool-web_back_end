0fc47f8758448bc3f4346a247c7b5d2a63f694cc
Gabriel <fossecruor@gmail.com>
2024-09-23 11:51:39 +0200

    no many

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,4 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    return mongo_collection.insert_many(kwargs)
+    res = mongo_collection.insert(kwargs)
+    return res.inserted_id
