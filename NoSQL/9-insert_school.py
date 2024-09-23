3d486b040cc53df3ad9e2756ec20497ddd5a60e4
Gabriel <fossecruor@gmail.com>
2024-09-23 11:56:45 +0200

    correct one

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    res = mongo_collection.insert(kwargs)
+    res = mongo_collection.insert_one(kwargs)
     return res.inserted_id
