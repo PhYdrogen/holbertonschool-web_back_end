7d1a3b1daf7137b40829fe709932375b08bf5b15
Gabriel <fossecruor@gmail.com>
2024-09-25 15:45:52 +0200

    forgot entries()

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const clone = [];
-  for (const e of array) {
+  for (const e of array.entries()) {
     clone[e[0]] = appendString + e[1];
   }
   return clone;
