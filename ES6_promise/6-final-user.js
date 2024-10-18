5264343388e2674c995e90644085e4a130e4068d
Gabriel <fossecruor@gmail.com>
2024-10-18 12:54:37 +0200

    fixed

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((listRes) => listRes.map((e) => (e.reason != null ? { status: e.status, value: e.reason }
+      : e)));
 }
