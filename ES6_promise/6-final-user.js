f1525efff7c17817c33fc8c64b5e6f713774f35d
Gabriel <fossecruor@gmail.com>
2024-10-18 12:56:57 +0200

    error to string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,6 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((listRes) => listRes.map((e) => (e.reason != null ? { status: e.status, value: e.reason }
+    .then((listRes) => listRes.map((e) => (e.reason != null
+      ? { status: e.status, value: e.reason.toString() }
       : e)));
 }
