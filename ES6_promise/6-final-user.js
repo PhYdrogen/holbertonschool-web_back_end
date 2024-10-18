d0ff5b6529f41fcb9d478714b802242a14cd213b
Gabriel <fossecruor@gmail.com>
2024-10-18 12:42:59 +0200

    allSettled nowere in da doc

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(() => {
-    // console.log(value);
-  }).catch(() => {
-    // console.error(error);
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
 }
