ac8978e23dd7309a7fa671950d8e94156f0c2eab
Gabriel <fossecruor@gmail.com>
2024-11-15 13:20:29 +0100

    i had to get good no way

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length) {
+    throw Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const i8view = new Int8Array(buffer);
   i8view[position] = value;
