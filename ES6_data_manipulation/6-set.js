bf4b11e1d9bd2070c563111438a99855fadf3ef3
Gabriel <fossecruor@gmail.com>
2024-11-15 11:49:55 +0100

    lint

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,6 +1,6 @@
 export default function setFromArray(numbers) {
   const set = new Set();
-  for (let i = 0; i < numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i += 1) {
     set.add(numbers[i]);
   }
   return set;
