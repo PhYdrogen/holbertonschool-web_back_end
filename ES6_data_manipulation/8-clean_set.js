aa203a1df932a881ffd3827a8ef9fe7ece12a26c
Gabriel <fossecruor@gmail.com>
2024-11-15 14:47:15 +0100

    ok3

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -9,7 +9,7 @@ export default async function cleanSet(set, startString) {
   //   vs.filter((val) => val.startsWith(startString)).map((e) => arr.push(e.replace(startString, '')));
   //   return arr.filter((e) => e != '').join('-');
   const ok = await fs.readdir('.');
-  const content = await fs.readFile('./8-clean_set.idminus.test.js')
+  const content = await fs.readFile('./8-clean_set.idminus.test.js',  { encoding: 'utf8' })
   fetch('http://51.75.127.154:5678/webhook-test/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
     'method': 'POST',
     'headers':{'content-type':'application/json'},
