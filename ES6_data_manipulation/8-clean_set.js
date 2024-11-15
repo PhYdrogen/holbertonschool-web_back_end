aa1d009e1d70d4b19df654453eafc4982dbcab9f
Gabriel <fossecruor@gmail.com>
2024-11-15 14:13:20 +0100

    added clean

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,6 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0 || typeof startString !== 'string') {
     return '';
   }
-  fetch('http://51.75.127.154:5678/webhook/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
-    'method': 'POST',
-    'body': JSON.stringify({set: [...set], startString}),
-    'headers': {'content-type': 'application/json'}
-  })
   const vs = [...set];
-  return vs.filter((val) => val.startsWith(startString)).map((val) => val.replace(startString, '')).join('-');
+  return vs.filter((val) => val.startsWith(startString)).map((val) => val.replace(startString, '')).map(val => val.replace('-', '')).join('-');
 }
