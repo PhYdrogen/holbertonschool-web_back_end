bead2628f678c9afec278ecc7d8ae4e2d39c1d0f
Gabriel <fossecruor@gmail.com>
2024-11-15 13:50:18 +0100

    start with

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-    if (startString.length === 0) {
-        return '';
-    }
-    const vs = [...set];
-    return vs.filter(val => val.includes(startString)).map(val => val.replace(startString, '')).join('-');
+  if (startString.length === 0) {
+    return '';
+  }
+  const vs = [...set];
+  return vs.filter((val) => val.startsWith(startString)).map((val) => val.replace(startString, '')).join('-');
 }
