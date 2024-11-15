5730e9df910eca8e96db1c7bf44efbc4f8fa81bd
Gabriel <fossecruor@gmail.com>
2024-11-15 11:48:09 +0100

    removed try

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const buffer = new ArrayBuffer(length);
-    const i8view = new Int8Array(buffer);
-    i8view[position] = value;
-    return new DataView(buffer);
-  } catch (e) {
-    throw e;
-  }
+  const buffer = new ArrayBuffer(length);
+  const i8view = new Int8Array(buffer);
+  i8view[position] = value;
+  return new DataView(buffer);
 }
