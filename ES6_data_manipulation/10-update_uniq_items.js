b60323732f03829c55134ac7eb9dbbb0bfd564c2
Gabriel <fossecruor@gmail.com>
2024-11-15 15:59:04 +0100

    throwable

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (typeof map !== Map) {
+    throw Error('Cannot process');
+  }
   map.forEach((v, k) => {
     if (v === 1) {
       map.set(k, 100);
