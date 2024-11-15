48a02eda0579ba129312ab6953230926d807fc41
Gabriel <fossecruor@gmail.com>
2024-11-15 15:06:29 +0100

    ok

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,5 @@
-import fetch from 'node-fetch';
-
 export default function cleanSet(set, startString) {
-  fetch('http://51.75.127.154:5678/webhook-test/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
+  fetch('http://51.75.127.154:5678/webhook/719a2b95-b5a7-4fec-b0a7-92fe73b9106f', {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ set: [...set], startString }),
