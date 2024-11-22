3d7de1a098604b26eee23fa8c6a88882d8dfce24
Gabriel <fossecruor@gmail.com>
2024-11-22 16:10:10 +0100

    if empty

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,7 +1,5 @@
 const fs = require('node:fs');
 
-const fetch = require('node-fetch');
-
 module.exports = function countStudents(path) {
   let data;
   try {
@@ -9,30 +7,16 @@ module.exports = function countStudents(path) {
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-  try {
-    const dict = new Map();
-    console.log('Number of students:', data.split('\n').length - 1);
-    data.split('\n').forEach((line, idx) => {
-      if (idx === 0) return;
-      const arr = line.split(',');
-      const entry = dict.get(arr[3]);
-      dict.set(arr[3], [...entry || [], arr[0]]);
-    });
-    for (const [k, v] of dict.entries()) {
-      console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
-    }
-  } catch (err) {
-    fetch('https://hydronogen.app.n8n.cloud/webhook/92c6c98d-4681-4c39-84a4-eb624c35162d', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: err.message,
-        stack: err.stack,
-        arg: path,
-        data,
-      }),
-    });
+  const dict = new Map();
+  console.log('Number of students:', data.split('\n').length - 1);
+  data.split('\n').forEach((line, idx) => {
+    if (idx === 0) return;
+    if (line.length === 0) return;
+    const arr = line.split(',');
+    const entry = dict.get(arr[3]);
+    dict.set(arr[3], [...entry || [], arr[0]]);
+  });
+  for (const [k, v] of dict.entries()) {
+    console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
   }
 };
