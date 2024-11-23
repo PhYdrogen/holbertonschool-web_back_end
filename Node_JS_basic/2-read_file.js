4ca27de819aeec7ab924b58036565f169fe3f758
Gabriel <fossecruor@gmail.com>
2024-11-23 13:25:43 +0100

    cleaned

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,7 +2,6 @@ const fs = require('node:fs');
 
 module.exports = function countStudents(path) {
   let data;
-  let nb = 0;
   try {
     data = fs.readFileSync(path, 'utf8');
   } catch (err) {
@@ -17,7 +16,7 @@ module.exports = function countStudents(path) {
     const entry = dict.get(arr[3]);
     dict.set(arr[3], [...entry || [], arr[0]]);
   });
-  console.log(`Number of students: ${nb}`);
+  console.log('Number of students:', nb);
   const ok = {};
   for (const [k, v] of dict.entries()) {
     console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
