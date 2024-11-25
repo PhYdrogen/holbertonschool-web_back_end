7ed7582006c4a92ddcb15df2e7e40f7a6b58431a
Gabriel <fossecruor@gmail.com>
2024-11-25 10:52:10 +0100

    t4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+import http from 'node:http';
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
+});
+app.listen(1245);
+module.exports = app;
