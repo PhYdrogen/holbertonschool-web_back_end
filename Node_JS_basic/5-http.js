5edab7b1e066c540f9b6989cc3748b13ce10760d
Gabriel <fossecruor@gmail.com>
2024-11-25 13:03:08 +0100

    sto sto sto

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,21 +1,26 @@
 const http = require('node:http');
-const DebugHolberton = require('./debug');
-const countStudents = require('./3-read_file_async');
 const process = require('node:process');
+const countStudents = require('./3-read_file_async');
 
-
-const d = new DebugHolberton();
-d.fetch();
 const app = http.createServer(async (req, res) => {
   if (req.url === '/students') {
-    await countStudents(process.argv[0]);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('This is the list of our students');
-  } else {
 
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');
-    }
+    countStudents(process.argv[2])
+      .then((result) => {
+        res.write('This is the list of our students\n');
+        res.write(result);
+      })
+      .catch(() => {
+        res.write('This is the list of our students\n');
+        res.write('Cannot load the database');
+      }).finally(() => {
+        res.end();
+      });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
 });
 app.listen(1245);
 module.exports = app;
