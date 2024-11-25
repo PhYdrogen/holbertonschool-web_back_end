0bcf47c2e3856e3cd0aa535a8a28e793d40d23ff
Gabriel <fossecruor@gmail.com>
2024-11-25 15:32:55 +0100

    t7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+const DebugHolberton = require('./debug');
+
+const app = express();
+const port = 1245;
+
+const d = new DebugHolberton();
+d.fetch();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  try {
+    const result = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${result}`);
+    res.status(200);
+  } catch (err) {
+    res.send('This is the list of our students\n');
+    res.send('Cannot load the database');
+    res.status(200);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
