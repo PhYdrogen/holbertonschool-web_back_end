782abd00a2313b94d9de7cb6c5c1c81c6ea59cf2
Gabriel <fossecruor@gmail.com>
2024-11-25 10:54:44 +0100

    added debug

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,5 +1,8 @@
 import http from 'node:http';
+import DebugHolberton from './debug';
 
+const d = new DebugHolberton();
+d.fetch();
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!\n');
