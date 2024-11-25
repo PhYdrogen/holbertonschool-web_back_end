e8bd365606ad7730d4377f2ccdf54b42a62c6ab7
Gabriel <fossecruor@gmail.com>
2024-11-25 10:44:16 +0100

    is good

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,27 +6,31 @@ module.exports = function countStudents(path) {
   d.fetch();
 
   return new Promise((resolve, rejet) => {
-    fs.readFile(path, 'utf8').then((data, err) => {
-      if (err) rejet(new Error('Cannot load the database'));
+    try {
+      fs.readFile(path, 'utf8').then((data) => {
+        let nb = 0;
+        const dict = new Map();
 
-      let nb = 0;
-      const dict = new Map();
-
-      data.split('\n').forEach((line, idx) => {
-        if (idx === 0) return;
-        if (line.length === 0) return;
-        nb += 1;
-        const arr = line.split(',');
-        const entry = dict.get(arr[3]);
-        dict.set(arr[3], [...entry || [], arr[0]]);
+        data.split('\n').forEach((line, idx) => {
+          if (idx === 0) return;
+          if (line.length === 0) return;
+          nb += 1;
+          const arr = line.split(',');
+          const entry = dict.get(arr[3]);
+          dict.set(arr[3], [...entry || [], arr[0]]);
+        });
+        console.log(`Number of students: ${nb}`);
+        const ok = {};
+        for (const [k, v] of dict.entries()) {
+          console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
+          ok[k] = v;
+        }
+        resolve();
+      }).catch(() => {
+        rejet(new Error('Cannot load the database'));
       });
-      console.log(`Number of students: ${nb}`);
-      const ok = {};
-      for (const [k, v] of dict.entries()) {
-        console.log(`Number of students in ${k}: ${v.length}. List: ${v.join(', ')}`);
-        ok[k] = v;
-      }
-      resolve();
-    });
+    } catch (err) {
+      rejet(new Error('Cannot load the database'));
+    }
   });
 };
