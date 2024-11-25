66c567f4a5f113d335762e3487c5b8e330672d38
Gabriel <fossecruor@gmail.com>
2024-11-25 11:06:11 +0100

    new line :)

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,10 +2,10 @@ const http = require('node:http');
 const DebugHolberton = require('./debug');
 
 const d = new DebugHolberton();
-d.fetch();
+// d.fetch();
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 app.listen(1245);
 module.exports = app;
