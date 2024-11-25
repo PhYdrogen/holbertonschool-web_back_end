804daee0435e3fe6f0f6e7709babaa25b8c619bb
Gabriel <fossecruor@gmail.com>
2024-11-25 11:07:16 +0100

    linter

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,5 @@
 const http = require('node:http');
-const DebugHolberton = require('./debug');
 
-const d = new DebugHolberton();
-// d.fetch();
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
