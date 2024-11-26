f9e03fe54c22be689e8ffb7c49c8cd8e33932b93
Gabriel <fossecruor@gmail.com>
2024-11-26 11:47:50 +0100

    add fetching

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import r from './routes/index';
+import DebugHolberton from '../debug';
+
+const d = new DebugHolberton();
+d.fetch();
 
 const app = express();
 app.use('/', r);
