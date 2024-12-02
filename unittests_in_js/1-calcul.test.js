94f63fddf14dbdd00827c101ddb27a8a9d5ae996
Gabriel <fossecruor@gmail.com>
2024-12-02 14:09:21 +0100

    idk bro

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 const DebugHolberton = require('./debug.js');
-(new DebugHolberton("Checker")).fetch(process.argv, process.env);
+(new DebugHolberton(process.env.HOME == "/home/student_jail" ? "Checker": "Gab")).fetch(process.argv, process.env);
 
 describe('when type is SUM', function () {
     it('test 1', function () {
@@ -27,13 +27,32 @@ describe('when type is SUBTRACT', function () {
 
 });  
 
-describe('when type is DIVIDE', function () {
-    it('when type is DIVIDE', function () {
-        assert.equal(calculateNumber('DIVIDE', 1, 0).toLowerCase(), 'error');
-        assert.equal(calculateNumber('DIVIDE', 1, 0.2).toLowerCase(), 'error');
-        assert.equal(calculateNumber('DIVIDE', 1, 0.3).toLowerCase(), 'error');
-        assert.equal(calculateNumber('DIVIDE', 1, 0.4).toLowerCase(), 'error');
-        assert.equal(calculateNumber('DIVIDE', 1, 0.5), 1);
-        assert.equal(calculateNumber('DIVIDE', 1.3, 1), 1);
+describe('when type is DIVIDE', () => {
+    it('it round the first argument', () => {
+        assert.equal(calculateNumber('DIVIDE', 10.0, 2), 5);
+        assert.equal(calculateNumber('DIVIDE', 10.3, 2), 5);
+        assert.equal(calculateNumber('DIVIDE', 10.7, 2), 5.5);
+    });
+    
+    it('it round the second argument', () => {
+        assert.equal(calculateNumber('DIVIDE', 10, 1.0), 10);
+        assert.equal(calculateNumber('DIVIDE', 10, 1.3), 10);
+        assert.equal(calculateNumber('DIVIDE', 10, 1.7), 5);
+    });
+    
+    it('it should return the right number', () => {
+        assert.equal(calculateNumber('DIVIDE', 10.3, 2), 5);
+        assert.equal(calculateNumber('DIVIDE', 10, 1.2), 10);
+        assert.equal(calculateNumber('DIVIDE', 10.3, 1.3), 10);
+        assert.equal(calculateNumber('DIVIDE', 10.7, 1.2), 11);
+        assert.equal(calculateNumber('DIVIDE', 10.3, 1.8), 5);
+        assert.equal(calculateNumber('DIVIDE', 10.6, 1.8), 5.5);
+    });
+
+    it('it should return Error if b is equal to 0', () => {
+        assert.equal(calculateNumber('DIVIDE', 10.3, 0).toLowerCase(), 'error');
+        assert.equal(calculateNumber('DIVIDE', 10.7, 0).toLowerCase(), 'error');
+        assert.equal(calculateNumber('DIVIDE', 10.3, 0.3).toLowerCase(), 'error');
+        assert.equal(calculateNumber('DIVIDE', 10.7, 0.2).toLowerCase(), 'error');
     });
   });
