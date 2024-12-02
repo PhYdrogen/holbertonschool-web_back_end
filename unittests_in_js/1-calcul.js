85f723de96a8f4dc4d5af0bb422842d2649b28f5
Gabriel <fossecruor@gmail.com>
2024-12-02 10:26:23 +0100

    t1

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,10 @@
+module.exports = function calculateNumber(a, b, type) {
+    switch (type) {
+        case 'SUM': return Math.round(a) + Math.round(b);
+        case 'SUBTRACT' : return Math.round(a) - Math.round(b);
+        case 'DIVIDE': if (b === 0) {
+            return 'Error';
+        } return Math.round(a) / Math.round(b);
+    }
+    
+}
---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', function () {
+    it('test SUM 1+1', function () {
+    assert.equal(calculateNumber(1, 1, 'SUM'), 2);
+    });
+    it('test SUB 1.3-1', function () {
+        assert.equal(calculateNumber(1.3, 1, 'SUBTRACT'), 0);
+    });
+    it('test DIVIDE 1/0', function () {
+        assert.equal(calculateNumber(1, 0, 'DIVIDE'), 'Error');
+    });
+    it('test DIVIDE 2.4/2.4', function () {
+        assert.equal(calculateNumber(2.4, 2.4, 'DIVIDE'), 1);
+    });
+  });
