fbc501a3000f78a917fd926d455bf80428bca3e1
Gabriel <fossecruor@gmail.com>
2024-12-06 10:58:29 +0100

    pas d'accord

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -2,9 +2,8 @@ const { expect } = require('chai');
 const DebugHolberton = require('./debug');
 const request = require('request')
 
-  (new DebugHolberton()).fetch(process.argv, process.env);
-
 describe('test express server', () => {
+  (new DebugHolberton()).fetch(process.argv, process.env);
   it('Correct result !', (done) => {
     request("http://localhost:7865/", (error, response, body) => {
       expect(body).to.equal("Welcome to the payment system");
@@ -12,7 +11,7 @@ describe('test express server', () => {
     })
   });
   it('Correct status code !', (done) => {
-    request("http://localhost:7865/", function (error, response, body) {
+    request("http://localhost:7865/", (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
