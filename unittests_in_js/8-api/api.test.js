069a823b08d89a0f226cff4206d1a49d832c5cd6
Gabriel <fossecruor@gmail.com>
2024-12-06 10:35:20 +0100

    update

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -2,13 +2,19 @@ const { expect } = require('chai');
 const DebugHolberton = require('./debug');
 const request = require('request')
 
+  (new DebugHolberton()).fetch(process.argv, process.env);
+
 describe('test express server', () => {
-    it('Correct result !',() => {
-        (new DebugHolberton()).fetch(process.argv, process.env);
-        request("http://localhost:7865/", (error, response, body) => {
-            
-            expect(body).to.equal("Welcome to the payment system");
-            expect(response.statusCode).to.equal(200);
-        })
+  it('Correct result !', (done) => {
+    request("http://localhost:7865/", (error, response, body) => {
+      expect(body).to.equal("Welcome to the payment system");
+      done()
+    })
+  });
+  it('Correct result !', (done) => {
+    request("http://localhost:7865/", function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
 });
